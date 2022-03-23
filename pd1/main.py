15ac41ca63d6c2bda24247cf8e5791fde8993c2e
MichalSpiewanowski <100282550+MichalSpiewanowski@users.noreply.github.com>
2022-03-23 01:48:44 +0100

    Add files via upload

---
+++ pd1/main.py
@@ -0,0 +1,74 @@
+#Zadanie 1
+a= 'Pierwszy'
+print(a)
+print(type(a))
+
+b = 2
+print(b)
+print(type(b))
+
+c = 1.3
+print(c)
+print(type(c))
+
+d = 7+1j
+print(d)
+print(type(d))
+
+#Zadanie 2
+e = 6
+f = 3
+print('a = ', e)
+print('b = ', f)
+dodawanie = e + f
+print('a + b = ', dodawanie)
+odejmowanie = e - f
+print('a - b = ', odejmowanie)
+mnozenie = e * f
+print('a * b = ', mnozenie)
+dzielenie = e / f
+print('a / b = ', dzielenie)
+potega = e ** f
+print('a ^ b = ', potega)
+#Zadanie 3
+h = 45
+print(h)
+h+=3
+print(h)
+h-=1
+print(h)
+h*=6
+print(h)
+h/=2
+print(h)
+h**=2
+print(h)
+h%=4
+print(h)
+
+#Zadanie 4
+import math
+print(math.e**10)
+print((math.log10(5+math.sin(8)**2))**(1/6))
+print(abs(3.55))
+print(abs(4.80))
+
+#Zadanie 5
+i = 'MICHAŁ'
+j = 'ŚPIEWANOWSKI'
+print(i.capitalize())
+print(j.capitalize())
+
+#Zadanie 6
+k = 'la la la la la la la la la'
+print(k.count('la'))
+
+#Zadanie 7
+l = 'Pszczółka'
+print(l[0])
+print(l[8])
+
+#Zadanie 8
+print(k.split(' '))
+
+#Zadanie 9
